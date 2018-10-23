58b41d19df6b82d36f0e6935d55505e7de4c6b68
David Miguel Lozano <me@davidmiguel.com>
2018-10-23 20:04:46 +0200

    Init project

---
+++ linechart/src/main/java/com/davidmiguel/linechart/LineChart.java
@@ -0,0 +1,28 @@
+package com.davidmiguel.linechart;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class LineChart extends View {
+
+    public LineChart(Context context) {
+        super(context);
+    }
+
+    public LineChart(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LineChart(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public LineChart(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
