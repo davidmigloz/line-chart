fde16ab07e4cb549dfe93f577057882ae4a1143a
David Miguel Lozano <me@davidmiguel.com>
2018-11-05 15:15:40 +0100

    Improve sample

--- sample/src/main/java/com/davidmiguel/linechart/sample/MainActivity.java
+++ sample/src/main/java/com/davidmiguel/linechart/sample/MainActivity.java
@@ -1,63 +1,45 @@
 package com.davidmiguel.linechart.sample;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
 
-import com.davidmiguel.linechart.LineChartFillType;
 import com.davidmiguel.linechart.LineChartAdapter;
-import com.davidmiguel.linechart.LineChartView;
-import com.davidmiguel.linechart.animation.MorphLineChartAnimator;
-import com.davidmiguel.linechart.touch.OnScrubListener;
-
-import java.util.Random;
+import com.davidmiguel.linechart.LineChartFillType;
+import com.davidmiguel.linechart.sample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+    private static final float[] DATA_ALL = new float[]{68, 22, 31, 57, 35, 79, 86, 47, 34, 55, 80, 72, 99, 66, 47, 42, 56, 64, 66, 80, 97, 10, 43, 12, 25, 71, 47, 73, 49, 36};
+    private static final float[] DATA_YEAR = new float[]{1, 10, 31, 32, 35, 79, 86, 47, 34, 55, 50, 50, 30, 39, 40, 20, 29, 39, 30, 30, 39, 38, 20, 18, 10, 12, 13, 10, 12, 11};
+    private static final float[] DATA_MONTH = new float[]{0, 1, 2, 3, 4, 5, 6, 7};
 
-    private LineChartView lineChartView;
-    private RandomizedAdapter adapter;
+    private ActivityMainBinding binding;
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        lineChartView = findViewById(R.id.line_chart);
+        adapter = new MyAdapter();
+        binding.lineChart.setAdapter(adapter);
+        binding.lineChart.setScrubListener(value -> binding.total.setText(value != null ? Float.toString((float) value) : ""));
+        binding.lineChart.setFillType(LineChartFillType.DOWN);
+//        MorphLineChartAnimator morphSparkAnimator = new MorphLineChartAnimator();
+//        morphSparkAnimator.setDuration(2000L);
+//        morphSparkAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+//        binding.lineChart.setLineChartAnimator(morphSparkAnimator);
 
-        adapter = new RandomizedAdapter();
-        lineChartView.setAdapter(adapter);
-        lineChartView.setScrubListener(value -> Log.d(TAG, "onScrubbed: " + value));
-
-        findViewById(R.id.random_button).setOnClickListener(view -> adapter.randomize());
-
-        lineChartView.setFillType(LineChartFillType.DOWN);
-        MorphLineChartAnimator morphSparkAnimator = new MorphLineChartAnimator();
-        morphSparkAnimator.setDuration(2000L);
-        morphSparkAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
-        lineChartView.setLineChartAnimator(morphSparkAnimator);
+        binding.allBtn.setOnClickListener(v -> adapter.setData(DATA_ALL));
+        binding.yearBtn.setOnClickListener(v -> adapter.setData(DATA_YEAR));
+        binding.monthBtn.setOnClickListener(v -> adapter.setData(DATA_MONTH));
     }
 
-    public static class RandomizedAdapter extends LineChartAdapter {
-        private final float[] yData;
-        private final Random random;
+    public static class MyAdapter extends LineChartAdapter {
 
-        RandomizedAdapter() {
-            random = new Random();
-            yData = new float[500 + random.nextInt(100)];
-            randomize();
-        }
-
-        void randomize() {
-            for (int i = 0, count = yData.length; i < count; i++) {
-                yData[i] = 50F + random.nextInt(50);
-            }
-            notifyDataSetChanged();
-        }
+        private float[] yData = DATA_ALL;
 
         @Override
         public int getCount() {
@@ -75,14 +57,9 @@ public class MainActivity extends AppCompatActivity {
             return yData[index];
         }
 
-        @Override
-        public boolean hasBaseLine() {
-            return true;
-        }
-
-        @Override
-        public float getBaseLine() {
-            return 0;
+        public void setData(float[] yData) {
+            this.yData = yData;
+            notifyDataSetChanged();
         }
     }
 }
